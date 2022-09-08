f60ca8ecc364e3bd536572c4f70fd893896b7479
Haashim <git@haash.im>
2022-09-08 17:40:12 +0100

    Framework For Handshake
    Laid the framework for completing the handshake with a server and clientside traversal of the tree in the same order with a subset of the nodes, specifically the replicatable ones. Also laid the framework for efficient retrieval of all replicatable StateNodes from the current working tree

--- src/lib/tree/definitions/index.ts
+++ src/lib/tree/definitions/index.ts
@@ -1,14 +1,14 @@
 import { StateTreeDefinition } from "../../global/Types";
 import { RestrictedScope, ScopeIndex } from "../nodes/RestrictedScope";
 import { IndexableNode, StateNode } from "../nodes/StateNode";
+import { VineNode } from "../nodes/Vine";
 
 export type BuiltDefinitions = {};
 
 export class Definition<T extends StateTreeDefinition> {
 	private internalDefinitions: T;
-	//private baseReplicatableNodes: StateNode[];
 
-	public constructor(definitions: T) {
+	public constructor(definitions: T, private baseReplicatableNodes: StateNode[]) {
 		this.internalDefinitions = definitions;
 	}
 
@@ -16,9 +16,11 @@ export class Definition<T extends StateTreeDefinition> {
 		return this.internalDefinitions[key as K];
 	}
 
-	public GetReplicatableNodes(): StateNode[] {
-		//AUGUMENT THE BASE REPLICATABLE NODES WITH THE NODES FROM
-		//BERRIES ON VINES
+	protected GetReplicatableNodes(): StateNode[] {
+		let nodes: StateNode[] = [];
+		nodes.push(...this.baseReplicatableNodes);
+		//AUGUMENT THE BASE REPLICATABLE NODES WITH THE NODES FROM BERRIES ON VINES
+		//SO JUST LOOP THROUGH THE BASE AND ADD ON THE VINE'S CHILDRENS
 		return [];
 	}
 }
@@ -28,16 +30,25 @@ export class DefinitionBuilder {
 		parent: StateNode,
 		definition: StateTreeDefinition,
 		currentScope: ScopeIndex,
+		replicatableNodes: StateNode[],
 	) {
-		for (let [i, v] of pairs(definition)) {
+		for (let [_, v] of pairs(definition)) {
 			if (v instanceof RestrictedScope) {
 				currentScope = v.getScope();
 			}
 
 			v.setParent(parent);
 
+			// IF NODE IS OF A REPLICATABLE SCOPE
+			// PUSH IT TO THE REPLICATABLE NODES
+
 			if (v instanceof IndexableNode) {
-				DefinitionBuilder.definitionTraversal(v, v.paths, currentScope);
+				DefinitionBuilder.definitionTraversal(
+					v,
+					v.paths,
+					currentScope,
+					replicatableNodes,
+				);
 			} else {
 				v.GetReplicator().setScope(currentScope);
 			}
@@ -45,15 +56,21 @@ export class DefinitionBuilder {
 	}
 
 	public static build<T extends StateTreeDefinition>(definition: T): Definition<T> {
+		let replicatableNodes: StateNode[] = [];
 		for (let [i, v] of pairs(definition)) {
 			if (v instanceof RestrictedScope) {
-				DefinitionBuilder.definitionTraversal(v, v.paths, v.getScope());
+				DefinitionBuilder.definitionTraversal(
+					v,
+					v.paths,
+					v.getScope(),
+					replicatableNodes,
+				);
 			} else {
 				warn(
 					`[ROAST] DefinitionBuilder: ${i} is not a RestrictedScope/Scope object.`,
 				);
 			}
 		}
-		return new Definition(definition);
+		return new Definition(definition, replicatableNodes);
 	}
 }
